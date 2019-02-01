895fb35c0be4badb29d3147046c51bca545c370b
ajeet kumar <ajeetx@email.com>
2019-02-02 00:28:56 +1100

    validation of triangle sides

--- api/service/triangleType.js
+++ api/service/triangleType.js
@@ -2,6 +2,10 @@ module.exports=(sides)=>{
 
     let side1=parseInt(sides[0]),side2=parseInt(sides[1]),side3=parseInt(sides[2]);
 
+    if(isNaN(parseInt(side1)) || isNaN(parseInt(side2)) || isNaN(parseInt(side3)))  {
+        return 'Error'
+    }
+
     if(side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2){
         return "Error";
     }
